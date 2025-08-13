c8db1fe6f871bbc025b3b60d41465c4c7b0e8d83
Sobitkhon-1 <ksobitkhon.@gmail.com>
2025-08-13 12:52:48 +0500

    Add ICar interface and Damas class implementation
    Introduces the ICar interface, CarType and Engine types, and a Damas class implementing ICar. Adds a sample usage of Damas and a servis function to demonstrate interface usage.

--- interface/src/main.ts
+++ interface/src/main.ts
@@ -1,5 +1,48 @@
 import './style.css'
 /*
+type CarType = 1 | 2 | 3 | 4
+type Engine = number
+
+interface ICar {
+ type: CarType
+ engine: Engine
+
+ move: () => number
+ stop: Function
+
+ airCondition?: boolean
+}
+
+class Damas implements ICar {
+
+ type: CarType = 1 // 1 = fuel, 2 = gasoline, 3 = electric, 4 = hybrid
+ engine: Engine = 1.5
+
+ move(): number {
+
+  return 1
+ }
+ stop() {}
+
+ dumlash() {}
+}
+
+const damas = new Damas()
+
+damas.type = 4
+
+servis( damas )
+
+function servis( car: ICar ) {
+
+ console.log( car.type )
+
+ const speed: number = car.move()
+
+ console.log( speed / 1_000 )
+}
+////
+
 const [min, max] = minmax([1, 2, 3, 4]);
 
 function minmax(arr: number[]) {
@@ -7,7 +50,6 @@ function minmax(arr: number[]) {
 }
 console.log('Min:', min, 'Max:', max);
 */
-
 
 const r = fn(1, 2);
 function fn(a: number, b: number) {
